79e6ac21eea95dbc03c10ca95bbd0fc39a591def
Farhaan <59924029+0xfarhaan@users.noreply.github.com>
2023-02-07 17:22:59 +0000

    feat: Test setup helpers (#4)
    * feat: Test setup helpers
    
    * feat: Use makeAddr as this labels address as well for traces
    
    * style: New line
    
    * fix: Update based on PR comments
    
    * fix: Update imports
    
    * style: Remove space
    
    * refactor: New test file layout

--- tests/TestBase.sol
+++ tests/TestBase.sol
@@ -3,12 +3,47 @@ pragma solidity 0.8.7;
 
 import { Test } from "../modules/forge-std/src/Test.sol";
 
+import { MapleLoanHarness } from "./harnesses/MapleLoanHarness.sol";
+
+import { MapleGlobalsMock, MockFactory } from "./mocks/Mock.sol";
+
 contract TestBase is Test {
+
+    address internal globals;
+    address internal governor;
+    address internal mockFactory;
 
     uint256 internal start;
 
     function setUp() public virtual {
+        governor    = makeAddr("governor");
+        globals     = address(new MapleGlobalsMock(governor));
+        mockFactory = address(new MockFactory(globals));
+
         start = block.timestamp;
     }
-    
+
+    function createLoan(
+        address borrower,
+        address lender,
+        address fundsAsset,
+        uint32[3] memory termDetails,
+        uint256[3] memory rates
+    ) internal returns (address loan_) {
+        MapleLoanHarness loan = new MapleLoanHarness();
+
+        loan.__setBorrower(borrower);
+        loan.__setLender(lender);
+        loan.__setFactory(mockFactory);
+        loan.__setFundsAsset(fundsAsset);
+        loan.__setGracePeriod(termDetails[0]);
+        loan.__setNoticePeriod(termDetails[1]);
+        loan.__setPaymentInterval(termDetails[2]);
+        loan.__setInterestRate(rates[0]);
+        loan.__setLateFeeRate(rates[1]);
+        loan.__setLateInterestPremium(rates[2]);
+
+        return address(loan);
+    }
+
 }
---
+++ tests/mocks/Mock.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.7;
+
+contract MapleGlobalsMock {
+
+    address public governor;
+    address public mapleTreasury;
+    address public securityAdmin;
+
+    bool public protocolPaused;
+
+    mapping(address => bool) public isBorrower;
+    mapping(address => bool) public isCollateralAsset;
+    mapping(address => bool) public isPoolAsset;
+
+    mapping(address => uint256) public platformOriginationFeeRate;
+    mapping(address => uint256) public platformServiceFeeRate;
+
+    bool internal _isFactory;
+
+    constructor(address governor_) {
+        governor   = governor_;
+        _isFactory = true;
+    }
+
+    function isFactory(bytes32, address) external view returns (bool) {
+        return _isFactory;
+    }
+
+    function setMapleTreasury(address mapleTreasury_) external {
+        mapleTreasury = mapleTreasury_;
+    }
+
+    function setSecurityAdmin(address securityAdmin_) external {
+        securityAdmin = securityAdmin_;
+    }
+
+    function setPlatformOriginationFeeRate(address poolManager_, uint256 feeRate_) external {
+        platformOriginationFeeRate[poolManager_] = feeRate_;
+    }
+
+    function setPlatformServiceFeeRate(address poolManager_, uint256 feeRate_) external {
+        platformServiceFeeRate[poolManager_] = feeRate_;
+    }
+
+    function setProtocolPaused(bool paused_) external {
+        protocolPaused = paused_;
+    }
+
+    function setValidBorrower(address borrower_, bool isValid_) external {
+        isBorrower[borrower_] = isValid_;
+    }
+
+    function setValidCollateralAsset(address collateralAsset_, bool isValid_) external {
+        isCollateralAsset[collateralAsset_] = isValid_;
+    }
+
+    function setValidPoolAsset(address poolAsset_, bool isValid_) external {
+        isPoolAsset[poolAsset_] = isValid_;
+    }
+
+    function __setGovernor(address governor_) external {
+        governor = governor_;
+    }
+
+    function __setIsFactory(bool isFactory_) external {
+        _isFactory = isFactory_;
+    }
+
+}
+
+contract MockFactory {
+
+    address public mapleGlobals;
+
+    constructor(address mapleGlobals_) {
+        mapleGlobals = mapleGlobals_;
+    }
+
+    function setGlobals(address globals_) external {
+        mapleGlobals = globals_;
+    }
+
+    function upgradeInstance(uint256, bytes calldata arguments_) external {
+        address implementation = abi.decode(arguments_, (address));
+
+        ( bool success, ) = msg.sender.call(abi.encodeWithSignature("setImplementation(address)", implementation));
+
+        require(success);
+    }
+
+}
